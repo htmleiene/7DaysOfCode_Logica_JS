681a8ace2c8d5e1fe7f4ef83bfc98d7efb9f91d0
Leiene Neves <leiene.neves@gmail.com>
2024-07-17 15:36:00 -0300

    day2

---
+++ dia2.js
@@ -0,0 +1,8 @@
+window.onload = function ask () {
+    var name = prompt('Olá, qual o seu nome?');
+    var age = prompt('Quantos anos você tem?');
+    var language = prompt('Qual linguagem de programação você está estudando?');
+    alert('Olá, ' + name + ', você tem' + age + ' anos de idade e já está aprendendo ' + language + '!');
+
+}
+
