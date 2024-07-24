778e51f3bd8da2aea6cade2110791c77d2052cbe
Leiene Neves <leiene.neves@gmail.com>
2024-07-24 09:25:31 -0300

    change

--- dia2/dia2.js
+++ dia2/dia2.js
@@ -1,8 +1,30 @@
-window.onload = function ask () {
-    var name = prompt('Olá, qual o seu nome?');
-    var age = prompt('Quantos anos você tem?');
-    var language = prompt('Qual linguagem de programação você está estudando?');
-    alert('Olá, ' + name + ', você tem' + age + ' anos de idade e já está aprendendo ' + language + '!');
+document.addEventListener("DOMContentLoaded", function() {
+    const houseIcon = document.querySelector("#house-icon");
 
+    houseIcon.addEventListener("mouseover", function() {
+        houseIcon.classList.remove("bi-house");
+        houseIcon.classList.add("bi-house-fill");
+    });
+
+    houseIcon.addEventListener("mouseout", function() {
+        houseIcon.classList.remove("bi-house-fill");
+        houseIcon.classList.add("bi-house");
+    });
+});
+
+window.onload = function() {
+    const form = document.getElementById('form');
+    const result = document.getElementById('result');
+    const resultMessage = document.getElementById('resultMessage');
+
+    form.onsubmit = function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const age = document.getElementById('age').value;
+        const language = document.getElementById('language').value;
+
+        resultMessage.textContent = `Olá, ${name}! Você tem ${age} anos de idade e já está aprendendo ${language}!`;
+        result.classList.remove('hidden');
+    };
 }
-
